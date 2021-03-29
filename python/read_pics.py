ea7096f0979eacf47918e74e9d1b279d1faf3b32
Etienne Sharpin <etienne.sharpin@epita.fr>
2021-03-29 16:09:47 +0200

    ADD: get_dataset()

--- python/read_pics.py
+++ python/read_pics.py
@@ -6,53 +6,72 @@ Script python pour ouvrir les fichiers de traces de clavier
 import matplotlib.pyplot as plt
 import numpy as np
 import time
+import os
+
 
 def read_int(f):
     ba = bytearray(4)
     f.readinto(ba)
     prm = np.frombuffer(ba, dtype=np.int32)
     return prm[0]
-    
+
+
 def read_double(f):
     ba = bytearray(8)
     f.readinto(ba)
     prm = np.frombuffer(ba, dtype=np.double)
     return prm[0]
 
+
 def read_double_tab(f, n):
-    ba = bytearray(8*n)
+    ba = bytearray(8 * n)
     nr = f.readinto(ba)
     if nr != len(ba):
         return []
     else:
         prm = np.frombuffer(ba, dtype=np.double)
         return prm
-    
+
+
 def get_pics_from_file(filename):
     # Lecture du fichier d'infos + pics detectes (post-processing KeyFinder)
-    print("Ouverture du fichier de pics "+filename)
     f_pic = open(filename, "rb")
     info = dict()
     info["nb_pics"] = read_int(f_pic)
-    print("Nb pics par trame: " + str(info["nb_pics"]))
     info["freq_sampling_khz"] = read_double(f_pic)
-    print("Frequence d'echantillonnage: " + str(info["freq_sampling_khz"]) + " kHz")
     info["freq_trame_hz"] = read_double(f_pic)
-    print("Frequence trame: " + str(info["freq_trame_hz"]) + " Hz")
     info["freq_pic_khz"] = read_double(f_pic)
-    print("Frequence pic: " + str(info["freq_pic_khz"]) + " kHz")
     info["norm_fact"] = read_double(f_pic)
-    print("Facteur de normalisation: " + str(info["norm_fact"]))
     tab_pics = []
     pics = read_double_tab(f_pic, info["nb_pics"])
     nb_trames = 1
     while len(pics) > 0:
-        nb_trames = nb_trames+1
+        nb_trames = nb_trames + 1
         tab_pics.append(pics)
         pics = read_double_tab(f_pic, info["nb_pics"])
     print("Nb trames: " + str(nb_trames))
     f_pic.close()
     return tab_pics, info
+
+
+def get_dataset():
+    """
+    returns the dataset in a dict with key : name of the key
+    value: list of spikes
+    """
+    dataset = {}
+    os.chdir('../data/')
+
+    for filename in os.listdir():
+        list_of_spikes, info = get_pics_from_file(filename)
+
+        # we get the name of the key by removing "pics_" at the beginning and ".bin" at the end
+        key = filename[5:-4]
+
+        dataset[key] = list_of_spikes
+
+    return dataset
+
 
 if __name__ == "__main__":
     pics_nokey, info = get_pics_from_file("../data/pics_NOKEY.bin")
@@ -62,7 +81,7 @@ if __name__ == "__main__":
     # NO KEY
     plt.figure(1)
     plt.subplot(211)
-    plt.plot(range(1,info["nb_pics"]+1), pics_nokey[0], 'ko')
+    plt.plot(range(1, info["nb_pics"] + 1), pics_nokey[0], 'ko')
     plt.xlabel('numéro de pic')
     plt.ylabel('valeur du pic')
     plt.title('no key')
@@ -70,7 +89,7 @@ if __name__ == "__main__":
     plt.grid(b=True, which='both')
     # PAD-0
     plt.subplot(212)
-    plt.plot(range(1,info["nb_pics"]+1), pics_pad0[0], 'ko')
+    plt.plot(range(1, info["nb_pics"] + 1), pics_pad0[0], 'ko')
     plt.xlabel('numéro de pic')
     plt.ylabel('valeur du pic')
     plt.title('PAD-0')
@@ -78,4 +97,3 @@ if __name__ == "__main__":
     plt.grid(b=True, which='both')
     #
     plt.show()
-    

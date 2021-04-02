b5d1e4b6d4d76db3eb1bb97b50f611748587b67f
Segolene Denjoy <sego.denjoy@gmail.com>
2021-04-02 15:06:47 +0200

    trame_show: ADD entry for genericity

--- python/trame_show.py
+++ python/trame_show.py
@@ -2,10 +2,6 @@ import matplotlib.pyplot as plt
 
 from dataset_manager import *
 
-
-train_dataset, test_dataset = get_dataset()
-d_list = list(test_dataset)
-
 def trame_show(X, key, figure_nbr):
     """Afficher les 100eres trames"""
     plt.figure(figure_nbr)
@@ -20,24 +16,27 @@ def trame_show(X, key, figure_nbr):
     plt.grid()
     plt.show()
 
-def trame_show_AllKey():
+def trame_show_AllKey(dataset):
     """Afficher les 100eres trames de toutes les keys"""
+    d_list = list(dataset)
     for i in range(len(d_list)):
-        trame_show(train_dataset.get(d_list[i]), d_list[i], i)
+        trame_show(dataset.get(d_list[i]), d_list[i], i)
 
-def trame_show_key(key):
+def trame_show_key(dataset, key):
     """Afficher les 100eres trames d'une key particulière
     key : str
     """
+    d_list = list(dataset)
     for i in range(len(d_list)):
         if (d_list[i] == key):
-            trame_show(train_dataset.get(d_list[i]), d_list[i], 0)
+            trame_show(dataset.get(d_list[i]), d_list[i], 0)
 
-def trame_show_result(trames: dict):
+def trame_show_result(dataset, trames: dict):
     print(list(trames.keys()))
+    d_list = list(dataset)
 
     for key in trames:
-        trame_show_key('NOKEY')
-        trame_show_key(key)
+        trame_show_key(dataset, 'NOKEY')
+        trame_show_key(dataset, key)
         trame_show(trames[key], f"{key} - LOGINMDP", 0)
         print('===========================')
